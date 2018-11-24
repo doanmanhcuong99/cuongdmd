ace6f8a5ad64eafe51c48d336bfdf1789ebe0ed5
doanmanhcuong99 <you@example.com>
2018-11-24 10:29:57 +0700

    first commit

---
+++ Exam/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierlID { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public string UnitslnStock { get; set; }
+        public string UnitOnOder { get; set; }
+        public string ReorderLevel { get; set; }
+        public string Discontinued { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+    }
+}
